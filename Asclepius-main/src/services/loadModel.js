4c6b1b3e375418028fff4449d19d2e17d90f643d
Febriyanto35 <128132189+Febriyanto35@users.noreply.github.com>
2024-05-22 19:13:04 +0800

    Delete Asclepius-main directory

--- Asclepius-main/src/services/loadModel.js
+++
@@ -1,9 +0,0 @@
-const tf = require("@tensorflow/tfjs-node");
-
-function loadModel() {
-  return tf.loadGraphModel(
-    "https://storage.googleapis.com/saved-model-mlgc/model.json"
-  );
-}
-
-module.exports = loadModel;
